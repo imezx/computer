4d42d0309f9142ab23c6333f4df83a224823a862
EternityDev <64758801+imezx@users.noreply.github.com>
2026-03-23 15:42:50 +0700

    Delete dist directory

--- dist/pluginTypes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * @file pluginTypes.ts
- * Type definitions for the LM Studio plugin controller.
- * Inferred from SDK patterns.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=pluginTypes.js.map
--- dist/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * @file types.ts
- * Shared type definitions for the computer plugin.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
